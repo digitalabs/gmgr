f8020567564b52e65d998431563ffdfb0bdbef6d
Nikki Carumba <n.carumba@irri.org>
2013-10-07 06:30:56 +0000

    main.php edited online with Bitbucket

--- protected/config/main.php
+++ protected/config/main.php
@@ -74,7 +74,7 @@ return array(
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
-		),*/
+		),
 		
 			'db'=>array(
 		    'class'=>'CDbConnection',
@@ -90,8 +90,8 @@ return array(
 		    'class'=>'CDbConnection',
 			'connectionString' => 'mysql:host=127.0.0.1;port=8005;dbname=iris_myisam_20121002',
 			'emulatePrepare' => true,
-			'username' => 'phenibquser',
-			'password' => 'phenibqpass',
+			'username' => 'root',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 		
