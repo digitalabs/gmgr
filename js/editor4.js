42a8933afee6dda414384837ce50551591d3576f
Kelly John Mahipus <k.mahipus@irri.org>
2013-11-22 14:22:23 +0800

    update save image function

--- js/editor4.js
+++ js/editor4.js
@@ -425,7 +425,7 @@ function writeDownloadLink()
         .attr("id", "download")
         .style("top", event.clientY+20+"px")
         .style("left", event.clientX+"px")
-        .html("<div style='position:fixed;left:40px;top:120px;'>Scroll down and right-click on the preview below and choose Save as<br/>Left-Click to dismiss<br /></div>")
+        .html("<div style='position:fixed;left:40px;top:120px;'>Scroll down and <b><i>right-click</i></b> on the preview below and choose Save image as... <br/><b><i>Left-click</i></b> to dismiss<br /></div>")
         .append("img")
         .attr("src", "data:image/svg+xml;base64,"+ btoa(html));
 
