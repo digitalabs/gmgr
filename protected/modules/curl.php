9dcc838033c975634c2e5e524f949e40a45887ce
Nikki Carumba <n.carumba@irri.org>
2013-12-09 21:35:10 +0800

    fix indention, curl.php

--- protected/modules/curl.php
+++ protected/modules/curl.php
@@ -11,10 +11,9 @@
  * @author ncarumba
  */
 class curl {
-	
-	//$inputGID = $_GET['inputGID'];
-    public function __construct() 
-	{
+
+    //$inputGID = $_GET['inputGID'];
+    public function __construct() {
         
     }
 
@@ -24,19 +23,19 @@ class curl {
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($handle);
         $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-          /*echo "<br>HTTP CODE ERROR: ".$code ."<br>";
+        /* echo "<br>HTTP CODE ERROR: ".$code ."<br>";
           //echo $jsonText;
           echo "<br> cURL: WELCOME! ".$response."</outercode>";
           echo '<br>RESULT: '.print_r($response,1);
-          echo "<br> END</outercode>";*/
-         
-		 /*if($code != 200){
-		        header("Location: /GMGR/index.php?r=site/contactUs");
-		 }*/
+          echo "<br> END</outercode>"; */
+
+        /* if($code != 200){
+          header("Location: /GMGR/index.php?r=site/contactUs");
+          } */
     }
 
     public function parse() {
-		//http://172.29.4.99:8083/ws/standardization/term/parse
+        //http://172.29.4.99:8083/ws/standardization/term/parse
         $url = "http://172.29.4.99:8083/ws/standardization/term/parse";
 
         $this->exec($url);
@@ -47,76 +46,76 @@ class curl {
         $this->exec($url);
     }
 
-     public function createGID() {
+    public function createGID() {
         $url = "http://172.29.4.99:8083/ws/standardization/term/createGID";
         $this->exec($url);
     }
-    
+
     public function chooseGID() {
         $url = "http://172.29.4.99:8083/ws/standardization/term/chooseGID";
         $this->exec($url);
     }
+
     public function updateMethod() {
         $url = "http://172.29.4.99:8083/ws/standardization/term/updateMethod";
         $this->exec($url);
     }
-	
-	public function show_germplasm_details() 
-	{
+
+    public function show_germplasm_details() {
         $url = "http://172.29.4.99:8080/ws/standardization/term/show_germplasm_details";
         $this->exec($url);
-		
-		$gid = $_POST['hidGID'];
-		$a = array('GID'=>$gid);
-		$data = json_encode($a);
-		$ch = curl_init();
-		$ch =curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                  
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-			'Content-Type: application/json',                                                                                
-			'Content-Length: ' . strlen($data))                                                                       
-		); 
-		
-		$result = curl_exec($ch);
-		$jsonfile = "trydocinfo.json";
-		json_decode($result, true);
+
+        $gid = $_POST['hidGID'];
+        $a = array('GID' => $gid);
+        $data = json_encode($a);
+        $ch = curl_init();
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data))
+        );
+
+        $result = curl_exec($ch);
+        $jsonfile = "trydocinfo.json";
+        json_decode($result, true);
     }
-	
-	public function searchGID() 
-	{
+
+    public function searchGID() {
         $url = "http://localhost:8080/ws/standardization/term/searchGID";
         $this->exec($url);
-		
-		$gid = $_POST['inputGID'];
-		$level = $_POST['maxStep'];
-		$a = array('GID'=>$gid,'LEVEL'=>$level);
-		//$b = array('LEVEL'=>$level);
-		$data = json_encode($a);
-		//$datab = json_encode($b);
-		$ch = curl_init();
-		$ch =curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                  
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-			'Content-Type: application/json',                                                                                
-			'Content-Length: ' . strlen($data))                                                                       
-		); 
-		
-		$result = curl_exec($ch);
-		$jsonfile = "trydocinfo.json";
-		json_decode($result, true);
-		//foreach($arr['GID'] as $GID){
-		//	file_put_contents($jsonfile, print_r($arr));
-			//echo "items: ". $GID;
-			//echo('<pre>');
-			//print_r($arr);
-			//echo('</pre>');
-		//};
-		//echo "success"; 
+
+        $gid = $_POST['inputGID'];
+        $level = $_POST['maxStep'];
+        $a = array('GID' => $gid, 'LEVEL' => $level);
+        //$b = array('LEVEL'=>$level);
+        $data = json_encode($a);
+        //$datab = json_encode($b);
+        $ch = curl_init();
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data))
+        );
+
+        $result = curl_exec($ch);
+        $jsonfile = "trydocinfo.json";
+        json_decode($result, true);
+        //foreach($arr['GID'] as $GID){
+        //	file_put_contents($jsonfile, print_r($arr));
+        //echo "items: ". $GID;
+        //echo('<pre>');
+        //print_r($arr);
+        //echo('</pre>');
+        //};
+        //echo "success"; 
     }
+
 }
 
 ?>
