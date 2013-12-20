db68155dbf6fdef214f98841f50ad8726c8ae673
Joanie Antonio <j.antonio@irri.org>
2013-12-20 09:49:52 +0800

    add another filterform

---
+++ protected/models/FilterPedigreeForm2.php
@@ -0,0 +1,30 @@
+<?php
+  class FilterPedigreeForm2 extends CFormModel{
+	  
+	  public $filters = array();
+	 
+	  public function __get($name){
+		  if(!array_key_exists($name, $this->filters))
+            $this->filters[$name] = null;
+        return $this->filters[$name];
+	  }
+	 /**
+     * Filter input array by key value pairs
+     * @param array $data rawData
+     * @return array filtered data array
+     */
+    public function filter(array $data)
+    {
+        foreach($data AS $rowIndex => $row) {
+            foreach($this->filters AS $key => $value) {
+                // unset if filter is set, but doesn't match
+                if(array_key_exists($key, $row) AND !empty($value)) {
+                    if(stripos($row[$key], $value) === false)
+                        unset($data[$rowIndex]);
+                }
+            }
+        }
+        return $data;
+    }
+  }
+?>
