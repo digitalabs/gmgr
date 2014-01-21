c54a3b2f85ab2f5ebb7c05f60aeb679b6ec3b20a
joantonio <joaniehappy@gmail.com>
2014-01-21 06:01:05 +0000

    importer.php edited online with Bitbucket

--- protected/views/site/importer.php
+++ protected/views/site/importer.php
@@ -65,7 +65,7 @@
                                 echo CHtml::error($model, 'file');
                             ?>
                             <?php
-                              echo CHtml::link('Download sample file with dates', Yii::app()->baseUrl. '/csv_files/germplasmList.csv');
+                              echo CHtml::link('Download sample file', Yii::app()->baseUrl. '/csv_files/germplasmList.csv');
                             ?>
                             <label>Location</label>
                             <?php
