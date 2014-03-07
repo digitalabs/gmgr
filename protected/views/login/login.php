2a1d79c6b9f501aa242868e4e0518dfa8e0692b6
Joanie Antonio <j.antonio@irri.org>
2014-03-07 14:51:26 +0800

    add db connection to the loginController

--- protected/views/login/login.php
+++ protected/views/login/login.php
@@ -67,3 +67,50 @@
         </div><!-- form -->
     </div>
 </body>  
+<script type='text/javascript'>
+    function storeLocal() {
+        if (<?php echo isset($database_details); ?>) {
+
+            if ('localStorage' in window && window['localStorage'] != null) {
+                try {
+                    var local_db_host = '<?php echo $database_details['local_db_host'] ?>';
+                    var local_db_name = '<?php echo $database_details['local_db_name'] ?>';
+                    var local_db_port = '<?php echo $database_details['local_db_port'] ?>';
+                    var local_db_username = '<?php echo $database_details['local_db_username'] ?>';
+                    var local_db_password = '<?php echo $database_details['local_db_password'] ?>';
+
+                    var central_db_host = '<?php echo $database_details['central_db_host'] ?>';
+                    var central_db_name = '<?php echo $database_details['central_db_name'] ?>';
+                    var central_db_port = '<?php echo $database_details['central_db_port'] ?>';
+                    var central_db_username = '<?php echo $database_details['central_db_username'] ?>';
+                    var central_db_password = '<?php echo $database_details['central_db_password'] ?>';
+
+                    localStorage.setItem('local_database_host', local_db_host);
+                    localStorage.setItem('local_database_name', local_db_name);
+                    localStorage.setItem('local_database_port', local_db_port);
+                    localStorage.setItem('local_database_username', local_db_username);
+                    localStorage.setItem('local_database_password', local_db_password);
+                    localStorage.setItem('central_database_host', central_db_host);
+                    localStorage.setItem('central_database_name', central_db_name);
+                    localStorage.setItem('central_database_port', central_db_port);
+                    localStorage.setItem('central_database_username', central_db_username);
+                    localStorage.setItem('central_database_password', central_db_password);
+
+                } catch (e) {
+                    if (e === QUOTA_EXCEEDED_ERR) {
+                        alert('Quota exceeded!');
+                    }
+                }
+            } else {
+                alert('Cannot store user preferences as your browser do not support local storage');
+            }
+        } else {
+        }
+    }
+
+    window.addEventListener('storage', storageEventHandler, false);
+    function storageEventHandler(event) {
+        storeLocal();
+    }
+
+</script>    
