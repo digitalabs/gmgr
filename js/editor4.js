0d76026d8ac980d9f165b5dc7444832ff3471ce6
kmahipus <k.mahipus@irri.org>
2014-03-11 08:45:07 +0000

    limit chars

--- js/editor4.js
+++ js/editor4.js
@@ -297,7 +297,7 @@ function drawChart2(node) {
                 .attr("dx", -19)
                 .attr("dy", -5)
                 //.limit('6','#left')
-                .attr("limit",6)
+                .attr("limit","6")
                 .text(node.name)
 				.attr("transform", function(d) {
 					 return "rotate(90)" 
