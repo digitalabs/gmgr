24bd1897b0ed45dd083d49dc05e87a35afd394f7
kmahipus <k.mahipus@irri.org>
2014-03-14 14:41:34 +0000

    editor4.js edited online with Bitbucket

--- js/editor4.js
+++ js/editor4.js
@@ -25,10 +25,10 @@ var data = (function () {
 		
 var realWidth = window.innerWidth;
 var realHeight = window.innerHeight;
-var margin = {top: 950, right: 50, bottom: 200, left: 1500},
+var margin = {top: 500, right: 50, bottom: 200, left: 500},
 	m = [100, 500, 100, 500],     
-    width = 2000 - margin.left - margin.right,
-    height = 5050 - margin.top - margin.bottom,
+    width = 10000 - margin.left - margin.right,
+    height = 10050 - margin.top - margin.bottom,
     h = realHeight -m[0] -m[2],
 	rectW = 200,
     rectH = 100,
@@ -54,7 +54,7 @@ var customNodes = new Array(),
 		 'transform':'rotate(270deg)'
 	});
 	
-    tmpNodes = d3.layout.tree().size([1300, 500]).nodes(data)
+    tmpNodes = d3.layout.tree().size([1300, 1000]).nodes(data)
 				 //.on("click", click)	;//;
 	
 					
@@ -62,7 +62,7 @@ var customNodes = new Array(),
     depencencyChart = d3.select("#graphDiv").append("svg:svg")
 			//.data(d3.entries(orientation))
             .attr("width", 10000)
-            .attr("height", 7500)
+            .attr("height", 10000)
             .append("svg:g")
 			.attr("class","drawarea")
 			.attr("transform", "translate(1000, 900)")
@@ -296,11 +296,22 @@ function drawChart2(node) {
 				.attr("class", "name")
                 .attr("dx", -19)
                 .attr("dy", -5)
-                .text(node.name)
+                .attr("text-overflow","ellipsis")
+                .attr("width", "6px")
+                .attr("white-space","nowrap")
+                .attr("overflow","hidden")
+                //.limit('6','#left')
+                //.attr("limit","6")
+                .text(node.name2)
 				.attr("transform", function(d) {
-					return "rotate(90)" 
+					 return "rotate(90)" 
 				})
 				.on("click", function(d,i) { click(node); })
+                //.on("mouseover", function (d) { d3.select(this).select("text")
+                //                                  .text(function(d){
+                //                                            return d.name2;
+                 //                                       })
+                 //                })
 				.on("mouseup", legendclick )
 				setTimeout(function () {
         //drawLegend();
@@ -344,11 +355,8 @@ function drawChart2(node) {
                     .attr("transform", "translate(" + (txtW+10) + ",0)")
 					.attr("cx", -30);
 			//}
-					
         }
-		
 		//}
-		
     });
 }
 
